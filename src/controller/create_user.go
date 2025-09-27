9cf9346799960b77ccfc8164a81a92e51198c1f0
Matheus Garcia <matheusgarciadg@gmail.com>
2025-09-27 17:39:12 -0300

    feat: validation fields, cause arrays

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/MatheusJusto/go-crud/src/config/rest_err"
+	"github.com/MatheusJusto/go-crud/src/config/validation"
 	"github.com/MatheusJusto/go-crud/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -14,7 +14,7 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorect field, error%s\any", err.Error()))
+		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
