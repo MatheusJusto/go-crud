c5926fc2db7efeb480e2857397ab816814ea8dfa
Matheus Garcia <matheusgarciadg@gmail.com>
2025-09-28 21:47:20 -0300

    feat: logger created, added zap log

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,25 +1,41 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/MatheusJusto/go-crud/src/config/logger"
 	"github.com/MatheusJusto/go-crud/src/config/validation"
 	"github.com/MatheusJusto/go-crud/src/controller/model/request"
+	"github.com/MatheusJusto/go-crud/src/controller/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"),
+	)
 
 	var userRequest request.UserRequest
-
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createdUser"),
+		)
 		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 
-	c.JSON(http.StatusCreated, gin.H{
-		"message": fmt.Sprintf("operation successfull"),
-	})
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	logger.Info("User created successfully",
+		zap.String("journey", "createdUser"),
+	)
+
+	c.JSON(http.StatusCreated, response)
 }
